eb87075626b5f036d85c3daeeacf1f720de329c1
robielcpnv <robiel.tesfazghi@cpnv.ch>
2022-03-28 12:05:39 +0200

    Add test for inexiting file

---
+++ src/tests/fileHelper.test.js
@@ -0,0 +1,24 @@
+/**
+ * @file      fileHelper.test.js
+ * @brief     This class is designed to test the behaviour of an fileHelper.
+ * @author    Created by Robiel.Tesfazghi
+ * @version   28-MAR-2022
+ */
+
+"use strict";
+
+const FileHelper =  require('../FileHelper/FileHelper');
+const FileNotFoundException =  require('../FileHelper/FileNotFoundException');
+
+test('Constructor_InexistingFile_ThrowException', () => {
+    //given
+    let wrongPath = "falkjalj";
+    let wrongFileName = "wrong.csv";
+
+    //when
+    expect(() => new FileHelper(wrongPath, wrongFileName)).toThrow(FileNotFoundException);
+
+    //then
+    //exception thrown
+
+})
