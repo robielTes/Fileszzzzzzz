254be961bbfd1267b08bff0dee0560f69d73e9b0
robielcpnv <robiel.tesfazghi@cpnv.ch>
2022-03-28 12:04:28 +0200

    Add FileHelper.js and FileNotFoundException

---
+++ src/FileHelper/FileHelper.js
@@ -0,0 +1,13 @@
+/**
+ * @file      FileHelper.js
+ * @brief     This class is designed to manage a FileHelper.
+ * @author    Created by Robiel.Tesfazghi
+ * @version   28-MAR-2022
+ */
+
+"use strict";
+
+const FileNotFoundException =  require('../FileHelper/FileNotFoundException');
+
+module.exports = class FileHelper {
+}
